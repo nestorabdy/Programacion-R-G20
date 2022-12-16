a076cf1d820633f9db2e576a499c618e890a805b
jpandob <41022387+jpandob@users.noreply.github.com>
2022-12-16 12:41:21 -0600

    Add files via upload

--- Postwork 08/punto6_RegresionLineal_postWork8.R
+++ Postwork 08/punto6_RegresionLineal_postWork8.R
@@ -64,6 +64,7 @@ dfLimpio <- df[complete.cases(df),]
 str(dfLimpio)
 summary(dfLimpio)
 
+
 #La primera parte es obtener una matriz de correlación manteniendo IA como
 #variable a comparar
 df <- read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2022/main/Sesion-08/Postwork/inseguridad_alimentaria_bedu.csv")
@@ -80,6 +81,7 @@ summary(dfLimpio)
 library(dplyr)
 library(ggplot2)
 
+#Se genra una matriz dxe correlación que incluye a IA
 df.select <- select(df, IA,nse5f, numpeho, edadjef, añosedu, ln_als, ln_alns)
 round(cor(df.select),4)
 
@@ -94,7 +96,7 @@ ln_alns      NA      NA      NA      NA      NA     NA       1"
 
 
 #Partiendo del DataFrame "limpio" o sin datos de "NA" se procede a la generación de una matriz de correlación
-
+" sin incluir a IA y así comparando con la tabla anterior"
 #adv.select <- select(adv, Sales,TV, Radio, Newspaper)
 #round(cor(adv.select),4)
 
@@ -115,17 +117,31 @@ ln_als  0.3566  0.3109 -0.1127  0.2421  1.0000  0.3285
 ln_alns 0.2922  0.0809 -0.0984  0.2184  0.3285  1.0000"
 
 # Teniendo las siguientes observaciones:
+
+#1. Hay una correlación débil entre El nivel socioeconómico del hogar y los gastos en alimentos
+# saludables y no saludables (0.3566 y 0.2922, respectivamente)
+#2. Hay una correlación prácticamente positiva moderada entre el Nivel Socioeconómico y los años de educación
+# del jefe de familia
+
 # Recordando que
 
 ##nse5f (Nivel socieconómico del hogar): 1 "Bajo", 2 "Medio bajo", 3 "Medio", 4 "Medio alto", 5 "Alto""
 #ln_als (Logarítmo natural del gasto en alimentos saludables)
 #ln_alns (Logarítmo natural del gasto en alimentos no saludables)
+# A pesar de que no se incluye IA se puede observar que existe una correlación positiva débil entre el Nivel 
+# Socioeconómico y de añosedu de 0.433, también con gasto de alimentos no slaudables que es de 0.2922
 
-##
 # A continuación se genera una matriz de dispersión relacionando las variables a través de ~
+# Como son muchos datos se divide en dos presentaciones gráficas
 
-pairs(~ nse5f +numpeho+edadjef+añosedu+ln_als+ln_alns, 
+pairs(~ IA+nse5f +ln_als+ln_alns,
       data = dfLimpio, gap = 0.4, cex.labels = 1.5)
+
+pairs(~ IA+nse5f+area+numpeho+refin +ln_als+ln_alns,
+      data = dfLimpio, gap = 0.4, cex.labels = 1.5)
+
+
+
 
 # Estimación del modelo de regresión, lineal o logístico, para identificiar los determinanres de la 
 #inseguridad alimentaria en México
@@ -133,3 +149,60 @@ pairs(~ nse5f +numpeho+edadjef+añosedu+ln_als+ln_alns,
 #Se inician con modelos de Regresión Lineal
 attach(dfLimpio) 
 modelo1 <- lm(IA ~ nse5f +numpeho+edadjef+añosedu+ln_als+ln_alns)
+summary(modelo1)
+
+"Residuals:
+    Min      1Q  Median      3Q     Max 
+-1.1896 -0.4883  0.1806  0.3145  0.6616 
+
+Coefficients:
+              Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  1.0816979  0.0317467  34.073  < 2e-16 ***
+nse5f       -0.0673001  0.0026281 -25.608  < 2e-16 ***
+numpeho      0.0283756  0.0017439  16.271  < 2e-16 ***
+edadjef     -0.0001502  0.0002240  -0.670    0.503    
+añosedu     -0.0103401  0.0007980 -12.958  < 2e-16 ***
+ln_als      -0.0105453  0.0051347  -2.054    0.040 *  
+ln_alns     -0.0218575  0.0031499  -6.939 4.07e-12 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 0.4303 on 20273 degrees of freedom
+Multiple R-squared:  0.09867,	Adjusted R-squared:  0.0984 
+F-statistic: 369.9 on 6 and 20273 DF,  p-value: < 2.2e-16"
+
+# Regresión logística
+
+
+logistic.prueba1<-glm(IA ~ nse5f+area+numpeho+refin+edadjef+sexojef+añosedu+ln_als+ln_alns, data=dfLimpio,family = binomial())
+summary(logistic.prueba1)
+exp(coef(logistic.prueba1))
+
+"Mostrando la siguiente tabla"
+
+"(Intercept)       nse5f        area     numpeho       refin     edadjef     sexojef     añosedu      ln_als     ln_alns 
+ 19.3437901   0.6896405   0.9021626   1.1907381   1.4887174   1.0003946   1.1678150   0.9472396   0.9131167   0.9021909 "
+
+# Se puede interpretar que los recursos financieros distintos al ingreso laboral (refin) tienen más alta probabilidad
+# sobre la Insuficiencia Laboral y el segundo factor es el número de personas en el hogar
+# Se analiza específicamente con el Número de personas
+y<-df$IA
+x<-df$numpeho
+logistic.prueba1<-glm(y ~x,data=dfLimpio, family = binomial())
+summary(logistic.prueba1)
+exp(coef(logistic.prueba1))
+
+# Dando los siguiente resultados:
+
+"(Intercept)           x 
+1.728377    1.132376 "
+
+" y se genra la gráfica para representar la relación"
+plot(IA ~ numpeho, data=dfLimpio, xlim = c(0,10))
+curve(predict(logistic.prueba1, newdata = data.frame(x), type = "response"),
+      add = TRUE)
+
+
+# Se concluye que la Insuficiencia Alimentaria le afecta el Número de personas del hogar
+
+
